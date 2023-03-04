3b25400f76f735b6ecb49914ca2c0de6d510c65a
Peter Viegas <peterviegas@outlook.com>
2023-03-04 22:23:36 +0000

    Change in the app.ts component for the understanding test by placing the router in the root.

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,5 @@ import { router } from "./routes/routes";
 export const app = express();
 
 app.use(express.json());
-app.use("/api/v1", router);
+//app.use("/api/v1", router);
+app.use("/", router);
