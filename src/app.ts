6c536670bc0c8c131522e155d252f3221c64ba7c
Peter Viegas <peterviegas@outlook.com>
2023-03-04 22:25:10 +0000

    Return from app.ts to original after understanding.

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,5 @@ import { router } from "./routes/routes";
 export const app = express();
 
 app.use(express.json());
-//app.use("/api/v1", router);
-app.use("/", router);
+app.use("/api/v1", router);
+//app.use("/", router);
