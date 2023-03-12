04b46575c0b1cb2cb9907b3d46cd8f3addee7b58
Peter Viegas <peterviegas@outlook.com>
2023-03-12 22:27:27 +0000

    Connecting your Minimalist Book Manager API to a PostgreSQL Database -Adding the PostgreSQL Dependency

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
+import * as dotenv from "dotenv";
+const environment = process.env.NODE_ENV || "dev";
+dotenv.config({ path: `.env.${environment}` });
+
 import { app } from "./app";
 import { populateDummyData } from "./database/database_seed";
+import { Sequelize } from 'sequelize';
+console.log(process.env.NODE_ENV)
+console.log(process.env.PORT)
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_USERNAME)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_DIALECT)
 
-const environment = process.env.NODE_ENV || "dev";
-const PORT = 3000;
+
+
+
+
+
+
+
+//const environment = process.env.NODE_ENV || "dev";
+//const PORT = 3000;
+const PORT = process.env.PORT;
+console.log("Porta:", PORT)
 
 console.log(`🌍 Running in ${environment} environment`);
 
